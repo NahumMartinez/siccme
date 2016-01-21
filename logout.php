02d2171404e0041553b7ab78389c671d87fc61f5
unknown <nahmartinez@NAHMARTINEZPW7.indra.es>
2016-01-21 10:55:49 -0600

    NMA | BORRO LA DATA DE SICCME

--- logout.php
+++
@@ -1,5 +0,0 @@
-<?php require('inc/config.php'); require('inc/funciones.php');
-session_id();
-session_destroy();
-redir("logeo.php");
-?>
--- scripts/b_Utiles.php
+++
@@ -1,7 +0,0 @@
-<?php
-        // Establecer la zona horaria predeterminada a usar. Disponible desde PHP 5.1
-        date_default_timezone_set('UTC');
-        //Imprimimos la fecha actual dandole un formato
-        //echo date("Y-m-d");
-		$fecha = date("Y-m-d");
-?>
